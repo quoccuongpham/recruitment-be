178af827e2977fa491c293f96758ee033df68945
cuongpham <quoccuonga2st@gmail.com>
2023-09-04 11:38:07 +0700

    register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-
+const argon = require("argon2");
+const db = require("../../database/models/index");
 router.post("/login", (req, res) => {
     console.log(req.body);
     res.json({
@@ -7,4 +8,42 @@ router.post("/login", (req, res) => {
     });
 });
 
+router.post("/register", async (req, res) => {
+    const { user_type_id, email, password, contact_number } = req.body;
+    const check_email = await db.sequelize
+        .model("user_account")
+        .findOne({ where: { email: email } });
+    if (check_email === null) {
+        try {
+            const hash_password = await argon.hash(password);
+            await db.sequelize.model("user_account").create({
+                user_type_id,
+                email,
+                password: hash_password,
+                contact_number,
+            });
+        } catch (error) {
+            return res.json({
+                success: false,
+                message: "Error",
+            });
+        }
+        return res.json({
+            success: true,
+            message: "register successfully",
+        });
+    } else {
+        return res.json({
+            success: false,
+            message: "Email already exist",
+        });
+    }
+});
+
+// router.get("/account", async (req, res) => {
+//     const user = await db.sequelize.model("user_account").findAll();
+//     res.json({
+//         user,
+//     });
+// });
 module.exports = router;
